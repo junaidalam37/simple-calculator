4b382ced874c2895c1eb3c4acbad3927c3ba0e9c
junaidalam37 <sajidjunaid80@gmail.com>
2025-04-17 11:43:32 -0700

    Add files via upload

---
+++ scrept.js
@@ -0,0 +1,15 @@
+let result=document.getElementById("result");
+
+function getResult(value){
+    if(value==="="){
+        result.value=eval(result.value)
+    }
+    else{
+        result.value+=value;
+    }
+}
+
+function clearResult(){
+    result.value="";
+    
+}
